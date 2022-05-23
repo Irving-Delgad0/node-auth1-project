22d90b0dbb3e6e0ce1a3f372396335d6e41c28b7
Irving Delgado <rvngdelgado@gmail.com>
2022-05-23 18:02:17 -0500

    completed part of the project. ran into some issues

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,3 +1,9 @@
+const bcrypt = require('bcryptjs')
+const express = require('express')
+const Users = require('../users/users-model')
+
+const router = express.Router()
+const {checkPasswordLength, checkUsernameExists, checkUsernameFree} = require('./auth-middleware')
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 
@@ -24,6 +30,12 @@
     "message": "Password must be longer than 3 chars"
   }
  */
+router.post('/register', checkPasswordLength, checkUsernameFree, async (req, res) => {
+  const {username, password} = req.body
+  let hash = bcrypt.hashSync(password, 8)
+  let result = await Users.add({username, password: hash})
+  res.status(200).json(result)
+})
 
 
 /**
@@ -42,6 +54,19 @@
   }
  */
 
+  router.post('/login', checkUsernameExists, (req, res, next) => {
+    const {password} = req.body
+
+    // let user = await Users.findBy({username}).first()
+    if(bcrypt.compareSync(password, req.user.password)){
+      req.session.user = req.user;
+      res.status(200).json({message: `Welcome ${req.user.username}`})
+    } else {
+       next({status: 401, message: 'Invalid credentials'})
+    }
+})
+
+
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +83,18 @@
     "message": "no session"
   }
  */
-
+router.get('/logout', (req, res, next) => {
+  if(req.session.user != null) {
+    req.session.destroy()
+    res.status(200).json({message: 'you are now logged out'})
+  } else {
+    res.status(400).json({message:'you are already logged out'})
+  }
+})
  
+router.get('/getThing', (req,res) => {
+  res.json({thing: req.session.thing ?? null})
+})
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
+const session = require('express-session')
+
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -17,9 +21,26 @@ const cors = require("cors");
 
 const server = express();
 
+let sessionConfig = {
+  name: 'chocolatechip',
+  secret: 'my secret password',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false,
+    httpOnly: true, 
+  },
+  resave: false,
+  saveUninitialized: false
+}
+
+server.use(session(sessionConfig))
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
